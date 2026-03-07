38555b04876270bf8ce55712e339408ad4251a92
amani-magra <amani___@outlook.com>
2026-03-06 19:31:38 -0500

    Changes:
    -Designed the activity_main.xml
    -initialized the buttons in the MainActivity.java file.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    protected Button chlorine_button, pH_button, wlvl_button, temp_button;
+    protected TextView chlorine_textView, pH_textView, wlvl_textView, temp_textView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setupUI();
     }
 
     @Override
@@ -29,5 +40,25 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void setupUI() {
+
+        // Buttons init
+        chlorine_button = findViewById(R.id.chlorine_button);
+        pH_button = findViewById(R.id.pH_button);
+        wlvl_button = findViewById(R.id.wlvl_button);
+        temp_button = findViewById(R.id.temp_button);
+
+        // Buttons action (to be added)
+
+        // View init
+        chlorine_textView = findViewById(R.id.chlorine_textView);
+        pH_textView = findViewById(R.id.pH_textView);
+        wlvl_textView = findViewById(R.id.wlvl_textView);
+        temp_textView = findViewById(R.id.temp_textView);
+
+        // View content (to be added)
+
     }
 }
