b7df0ade22fdba80bc45b23ee4e675bf6f39642f
amani-magra <amani___@outlook.com>
2026-03-20 16:49:03 -0400

    - changed display fields for activity_profile.xml and activity_settings.xml.
    - Created fragment dialogs to confirm deletion of a profile and another to edit pool information.

---
+++ app/src/main/java/com/example/myapplication/View/editPoolInfoDialogFragment.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.View;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class editPoolInfoDialogFragment extends Fragment {
+
+    public editPoolInfoDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_edit_pool_info_dialog, container, false);
+
+
+        return view;
+    }
+}
